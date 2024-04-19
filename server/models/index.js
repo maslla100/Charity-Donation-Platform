2f9831afcc954b4935efabccffbb33bc8f5cf6a0
luis llamas <luis.llamas@maslla.com>
2024-04-18 21:28:39 -0500

    deleted index.js

--- server/models/index.js
+++
@@ -1,7 +0,0 @@
-const Charity = require('./Charity');
-const User = require('./User');
-const Donation = require('./Donation');
-const Feedback = require('./Feedback')
-
-
-module.exports = { User, Charity, Donation, Feedback };
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,8 @@
 const bcrypt = require('bcryptjs');
-const { User, Charity, Donation, Feedback } = require('../models/index');
+const Charity = require('./models/Charity');
+const User = require('./models/User');
+const Donation = require('./models/Donation');
+const Feedback = require('./models/Feedback')
 const { signToken } = require('../utils/auth');
 const { ApolloError } = require('apollo-server-express');
 const { AuthenticationError } = require('apollo-server-express');
