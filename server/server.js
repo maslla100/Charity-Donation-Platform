0110b8197058cbcfce8b3fed5009e8ce2cc669bf
luis llamas <luis.llamas@maslla.com>
2024-04-18 22:57:45 -0500

    10000

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ const User = require('./models/User');
 const Donation = require('./models/Donation');
 const Feedback = require('./models/Feedback');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const app = express();
 // Remaining setup...
 
