4fe33baa04b6be41b0b7d8bae0efd5b1753aa3b1
luis llamas <luis.llamas@maslla.com>
2024-04-18 20:21:13 -0500

    charoty

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,5 @@
-
-const User = require('./User');
 const Charity = require('./Charity');
+const User = require('./User');
 const Donation = require('./Donation');
 const Feedback = require('./Feedback')
 
