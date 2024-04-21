7b85a4a8576ad0533cc96659036212c7adece787
maslla100 <luis.llamas@maslla.com>
2024-04-20 23:13:02 -0500

    uri

--- client/src/utils/apolloClient.js
+++ client/src/utils/apolloClient.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 const httpLink = new HttpLink({
-  uri: 'http://localhost:4000/graphql',
+  uri: 'https://charity-donation-platform-l9s2.onrender.com//graphql',
 });
 
 const client = new ApolloClient({
